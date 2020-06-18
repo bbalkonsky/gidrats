ac8aa98f2ad11605946ea2fd1cdf6bcb95f7f304
bbalkon <bbalkonsky@gmail.com>
2020-06-18 23:05:27 +0500

    всякое

--- test_network.py
+++ test_network.py
@@ -1,7 +1,7 @@
 import pickle
 
-import pandas as pd
 from tensorflow import keras
+from tqdm import tqdm
 
 df = pickle.load(open('outputs/for_test.pkl', 'rb'))
 df = df[
@@ -9,10 +9,15 @@ df = df[
      'C2_v_P_zatr', 'C2_v_T_izm',
      'C1_a_P_tr', 'C1_a_P_zatr', 'C1_a_T_izm', 'C2_a_P_tr', 'C2_a_P_zatr', 'C2_a_T_izm', 'RSI_function_tr',
      'RSI_function_zatr', 'RSI_function_temp', 'history_tr', 'history_zatr', 'history_temp']]
-print(df.info())
 
-model = keras.models.load_model('outputs/model_bin.h5')
+model = keras.models.load_model('outputs/model.h5')
 
-test_predictions = model.predict(df).flatten()
-predictions_df = pd.DataFrame(test_predictions)
-predictions_df.to_excel('outputs/predictions.xlsx')
+
+for idx, row in tqdm(df.iterrows(), total=df.shape[0]):
+    model.predict(row.to_frame().T)
+#     вот тут делай чо хочешь
+
+
+# test_predictions = model.predict(df).flatten()
+# predictions_df = pd.DataFrame(test_predictions)
+# predictions_df.to_excel('outputs/predictions.xlsx')
